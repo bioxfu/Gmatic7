3d81ada63181c16a954e3e5bd67f95bf6c116705
Fu Xing <bio.xfu@gmail.com>
2018-11-01 13:22:46 +0800

    remove .1 in gene_id and gene_name

--- script/download_tair10_GFF.sh
+++ script/download_tair10_GFF.sh
@@ -3,7 +3,7 @@ wget -c ftp://ftp.arabidopsis.org/home/tair/Genes/TAIR10_genome_release/TAIR10_g
 
 cat TAIR10_GFF3_genes.gff |grep 'chromosome'|cut -f1,5 > ../../genome/tair10/tair10.chromSize
 
-gffread TAIR10_GFF3_genes.gff -T -o tair10.gtf
+gffread TAIR10_GFF3_genes.gff -T -o- | sed -r 's/\.[0-9]+"; gene_name/"; gene_name/' | sed -r 's/\.[0-9]+";$/";/' > tair10.gtf
 
 cat TAIR10_GFF3_genes.gff | awk '{if($3=="gene")print $1"\t"$4"\t"$5"\t"$9"\t"$7}'|sed 's/ID=//'|sed 's/;Note=/\t/'|sed -r 's/;.+\t/\t/'|sort -k1,1 -k2,2n > tair10_gene.bed
 
