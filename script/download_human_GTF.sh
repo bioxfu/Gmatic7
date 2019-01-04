d3947c44dbe567b21464637f5776344591c687b0
Fu Xing <bio.xfu@gmail.com>
2019-01-04 16:53:39 +0800

    update

--- script/download_human_GTF.sh
+++ script/download_human_GTF.sh
@@ -1,3 +1,3 @@
-wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M19/gencode.vM19.primary_assembly.annotation.gtf.gz
-zcat gencode.vM19.primary_assembly.annotation.gtf.gz > GRCm38_vM19.gtf
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_29/gencode.v29.primary_assembly.annotation.gtf.gz
+zcat gencode.v29.primary_assembly.annotation.gtf.gz > GRCh38_v29.gtf
 
