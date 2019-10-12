253c2bb866c6b4f1a29f9d08c7702b1c9254a8aa
Fu Xing <bio.xfu@gmail.com>
2019-10-12 09:59:33 +0800

    add gene2name

--- script/download_human_genome.sh
+++ script/download_human_genome.sh
@@ -4,3 +4,4 @@ zcat GRCh38.primary_assembly.genome.fa.gz > GRCh38.fa
 
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_29/gencode.v29.transcripts.fa.gz
 zcat gencode.v29.transcripts.fa.gz > GRCh38_trans.fa
+grep '>' GRCh38_trans.fa |tr '|' '\t'|cut -f2,6|sort|uniq > GRCh38_gene2name.tsv
--- script/download_mouse_genome.sh
+++ script/download_mouse_genome.sh
@@ -4,4 +4,5 @@ zcat GRCm38.primary_assembly.genome.fa.gz > GRCm38.fa
 
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M19/gencode.vM19.transcripts.fa.gz
 zcat gencode.vM19.transcripts.fa.gz > GRCm38_trans.fa
+grep '>' GRCm38_trans.fa |tr '|' '\t'|cut -f2,6|sort|uniq > GRCm38_gene2name.tsv
 
