b53d196e746178a5cdd0d7abedbc7943325f6ac9
Fu Xing <bio.xfu@gmail.com>
2019-12-23 11:09:05 +0800

    build TxDb for mouse

--- script/download_mouse_GTF.sh
+++ script/download_mouse_GTF.sh
@@ -1,3 +1,5 @@
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M19/gencode.vM19.primary_assembly.annotation.gtf.gz
 zcat gencode.vM19.primary_assembly.annotation.gtf.gz > GRCm38_vM19.gtf
 
+mkdir txdb
+Rscript ../../script/makeTxdb.R GRCm38_vM19.gtf txdb/GRCm38_vM19_txdb.sqlite GRCm38_vM19_merged_exons_length.tsv
